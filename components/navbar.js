6021d8a7ec8d80bc4e00a75881f8bf6ffa211c35
Keaton Robert Gallagher <keaton@keaton.local>
2019-11-05 16:19:00 -0700

    Added the routing for the remaining pages inside of the navigation using Link imported from next/link

--- components/navbar.js
+++ components/navbar.js
@@ -1,50 +1,58 @@
 import React from 'react';
-
+import Link from 'next/link';
 const navbar = () => {
 	return (
-			<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-				<div className="container">
-					<a className="navbar-brand" href="#">
-						Start Bootstrap
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-toggle="collapse"
-						data-target="#navbarResponsive"
-						aria-controls="navbarResponsive"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon" />
-					</button>
-					<div className="collapse navbar-collapse" id="navbarResponsive">
-						<ul className="navbar-nav ml-auto">
-							<li className="nav-item active">
-								<a className="nav-link" href="#">
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+			<div className="container">
+				<a className="navbar-brand" href="#">
+					Start Bootstrap
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarResponsive"
+					aria-controls="navbarResponsive"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarResponsive">
+					<ul className="navbar-nav ml-auto">
+						<li className="nav-item active">
+							<Link href="/">
+								<a className="nav-link">
 									Home
 									<span className="sr-only">(current)</span>
 								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link href="/About">
+								<a className="nav-link">
 									About
 								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link href="/Services">
+								<a className="nav-link">
 									Services
 								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link href="/Contact">
+								<a className="nav-link">
 									Contact
 								</a>
-							</li>
-						</ul>
-					</div>
+							</Link>
+						</li>
+					</ul>
 				</div>
-			</nav>
+			</div>
+		</nav>
 	);
 };
 
