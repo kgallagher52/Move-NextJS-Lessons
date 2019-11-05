036f48a6ef5c541d07b04775194afbc3f06cafb8
Keaton Robert Gallagher <keaton@keaton.local>
2019-11-05 11:40:18 -0700

    Back to a normal functional component

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
-import React from 'react';
+
 //Function component
 //Using multple lines reqire you to use brackets
-const About = () => {
-	const message = 'Hello World';
-	return React.createElement('div', null, 'I am generating this with createElement');
-};
+ const About = () => {
+   const message  = "Hello World"
+   return(
+     <h1>Hello About Page - {message}</h1>
+     )
+   }
 
-export default About;
+export default About
