2798effbfae71698e09d133e23a24b6333169238
Keaton Robert Gallagher <keaton@keaton.local>
2019-11-05 11:39:47 -0700

    creating elements with react in functional components

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,9 @@
+import React from 'react';
 //Function component
 //Using multple lines reqire you to use brackets
 const About = () => {
 	const message = 'Hello World';
-	return <h1>Hello About Page - {message}</h1>;
+	return React.createElement('div', null, 'I am generating this with createElement');
 };
 
 export default About;
