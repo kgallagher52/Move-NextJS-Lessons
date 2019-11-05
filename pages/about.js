46f3de47e6508554f0863e34f444a3c1714d5610
Keaton Robert Gallagher <keaton@keaton.local>
2019-11-05 11:36:59 -0700

    Adding multple lines in a functional components

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,8 @@
-
 //Function component
- const About = () => (
-  <h1>Hello About Page</h1>
-)
+//Using multple lines reqire you to use brackets
+const About = () => {
+	const message = 'Hello World';
+	return <h1>Hello About Page - {message}</h1>;
+};
 
-export default About
+export default About;
