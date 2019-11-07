a96a0e97ade780cf29bda66a97fa5c381cbcb03c
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-11-07 15:25:52 -0700

    Added GET for a single movie

--- actions/index.js
+++ actions/index.js
@@ -27,16 +27,7 @@ export const getMovies = () => {
 }
 
 export const getMovieById = (id) => {
-  return new Promise((resolve, reject) => {
-    const movieIndex = MOVIE_DATA.findIndex((movie) => {
-      return movie.id === id
-    })
-    const movie = MOVIE_DATA[movieIndex];
-    setTimeout(() => {
-      resolve(movie);
-      reject("Cannot fetch data on getMovieById"); // Error handling
-    }, 50);
-  })
+  return axios.get(`${BASE_URL}/api/v1/movies/${id}`).then(res => res.data)
 }
 
 /*____POSTS____*/
--- server/index.js
+++ server/index.js
@@ -11,40 +11,50 @@ const handle = app.getRequestHandler(); // Creating handlers handling our reques
 
 const moviesData = require('./data.json');
 
-app.prepare().then(() => {
-  // Compile our code
+app.prepare().then(() => { // Compile our code
 
   const server = express(); //Creating the express server
   server.use(bodyParser.json()) //Telling the server to use this middlware
 
   /*___GETS___*/
   server.get('/api/v1/movies', (req, res) => {
-    res.json(moviesData);
+    return res.json(moviesData);
   });
 
-  server.get('*', (req, res) => {
-    //* is handling all request's coming to our server.
-    //Next js is handling the requests for us and providing pages where we are navigating to.
-    return handle(req, res); //This is going to parse our route and handle the correct route it needs to go to.
-  });
+  server.get('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+
+    const movieIndex = moviesData.findIndex(m => m.id === id);
+    const movie = moviesData[movieIndex];
+
+    return res.json(movie);
+  })
+  /*____________*/
 
   /*___POSTS___*/
   server.post('/api/v1/movies', (req, res) => {
     const movie = req.body;
     res.json({ ...movie, createdTime: 'today', author: 'Keaton Gallagher' });
   })
+  /*____________*/
+
 
   /*___UPDATES___*/
-  server.patch('/api/v1/movies/:id', (req, res) => {
-    const { id } = req.params
-    res.json({ message: `Updating movie of id: ${id}` });
-  })
+  /*____________*/
 
   /*___DELETES___*/
   server.delete('/api/v1/movies/:id', (req, res) => {
     const { id } = req.params
     res.json({ message: `Deleting movie of id: ${id}` });
   })
+  /*____________*/
+
+
+  server.get('*', (req, res) => {
+    //* is handling all request's coming to our server.
+    //Next js is handling the requests for us and providing pages where we are navigating to.
+    return handle(req, res); //This is going to parse our route and handle the correct route it needs to go to.
+  });
 
   const PORT = process.env.PORT || 3000; // We are trying to get the port from enviorment variable if that does not exist use 3000
 
