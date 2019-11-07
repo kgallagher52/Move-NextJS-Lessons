72198c317461690f4ad49c1d7b9c61a46e776563
Keaton Robert Gallagher <keaton@keaton.local>
2019-11-07 11:32:41 -0700

    Made the modal close using ref on the modal

--- components/movieCreateForm.js
+++ components/movieCreateForm.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 const MovieCreateForm = (props) => {
-  // Fix uncontrolled data!
 	const [ form, setForm ] = useState({
 		name: '',
 		description: '',
--- components/sideMenu.js
+++ components/sideMenu.js
@@ -3,15 +3,18 @@ import MovieCreateForm from "./movieCreateForm";
 import { createMovie } from '../actions/'
 
 const sideMenu = ({categories}) => {
-const handleCreateMovie = (movie) => {
-  createMovie(movie).then((movies) => {
-    console.log(JSON.stringify(movies))
-  })
-}
+  let modal = null
+
+  const handleCreateMovie = (movie) => {
+    createMovie(movie).then((movies) => {
+      console.log(JSON.stringify(movies))
+      modal.closeModal()
+    })
+  }
 
 	return (
 		<>
-      <Modal hasSubmit={false}>
+      <Modal ref={e => modal = e} hasSubmit={false}>
         <MovieCreateForm handleFormSubmit={handleCreateMovie}/>
       </Modal>
 			<h1 className="my-4">Movie Types</h1>
